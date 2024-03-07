91fc2887201d0d429ac35e053c39319922543142
konrad101296 <konradslimak@o2.pl>
2024-03-07 17:26:22 +0100

    Add project files.

---
+++ Week2Lesson8ExerciseNr8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter a string of characters: ");
+string characters = Console.ReadLine();
+
+for (int i = characters.Length - 1; i <= characters.Length; i--)
+{
+    Console.Write(characters[i]);
+    if (i == 0)
+    {
+        break;
+    }
+}
